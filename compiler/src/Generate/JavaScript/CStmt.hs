2ecb701204c6da2ea5b27c6a6a391760587e5813
tomschiffmann <45202935+tomschiffmann@users.noreply.github.com>
2020-01-27 11:23:27 +0100

    Update CStmt.hs
    Es wurden die Expressions Bool und Null mit aufgenommen. Außer dem wurde der Block Ausdruck in pretty mit eingearbeitet. Dazu wurde die Funktion prettyExpr geschrieben.

--- compiler/src/Generate/JavaScript/CStmt.hs
+++ compiler/src/Generate/JavaScript/CStmt.hs
@@ -1,14 +1,17 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Generate.JavaScript.CStmt
-  (Expr(..), pretty,examplee, Stmt(..))
+  (Expr(..), pretty, Stmt(..))
   where
-
+import Data.Typeable
 
 -- Expressions
 data Expr
     = String String
     | Null
+    | Bool Bool
+
+
 
 
 -- STATEMENTS
@@ -16,14 +19,32 @@ data Expr
 data Stmt
     = Block [Stmt]
     | EmptyStmt
-    | Var String String
+    | Var String String Expr
+    
 
-examplee :: Stmt
-examplee = Var "str" "Hi"
 
 --Die Funktion soll ein Stmt nehmen und diesen in ein C-Program in Form eines Strings schreiben.
 pretty :: Stmt -> String
 pretty statement =
   case statement of
-    Var name expr ->
-       "string " ++ name ++ " = " ++ expr ++ ";\n"
+    Var dataType name expr ->
+      case dataType of
+        "String" ->
+          "string" ++ " " ++ name ++ " = " ++ (prettyExpr expr) ++ ";\n"
+
+        "Bool" ->
+          "bool" ++ " " ++ name ++ " = " ++ (prettyExpr expr) ++ ";\n"
+    Block array ->
+      unwords (map pretty array)
+
+
+--Die Funktion wandelt ein Argument des typen Expr in einen String um.
+prettyExpr :: Expr -> String
+prettyExpr expression =
+  case expression of
+    Bool bool ->
+     case bool of
+       True ->  "true"
+       False -> "false"
+    String string -> string
+    Null -> "null"
