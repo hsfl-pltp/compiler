1cc115b7b23e2262d454dae4618fe6342ae579fa
tomschiffmann <45202935+tomschiffmann@users.noreply.github.com>
2020-01-28 10:59:15 +0100

    Update Example.hs
    Anpassung an die Änderungen in der CStmt.hs Datei

--- compiler/src/Generate/JavaScript/Example.hs
+++ compiler/src/Generate/JavaScript/Example.hs
@@ -1,15 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Generate.JavaScript.Example
-  (test)
+  (test1)
   where
 
 --Beispiel Konstruktor um die pretty Funktion zu testen.
 import Generate.JavaScript.CStmt 
 
 
-example :: Stmt
-example = Block [Var "String" "str" (String "hi"), Var "Bool" "boolean" (Bool False)]
+example1 :: Stmt
+example1 = Block [Var "String" "str" (String "hi"), Var "Bool" "boolean" (Bool False), IfStmt (Prefix PrefixNot (Bool True)) (Var "String" "str" (String "hi")) (Var "String" "str" (String "hi")), Var "Double" "double1" (Double 1.0)]
+
 
 test1 :: String
-test1 = pretty example
+test1 = pretty example1
