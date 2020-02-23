972ed18ec87fb10e9fc3fc0d8b9687013190d4e8
arljba <33658992+arljba@users.noreply.github.com>
2020-02-23 12:54:28 +0100

    Format affected files

--- compiler/src/AST/Utils/Binop.hs
+++ compiler/src/AST/Utils/Binop.hs
@@ -1,23 +1,18 @@
 {-# OPTIONS_GHC -Wall #-}
+
 module AST.Utils.Binop
   ( Precedence(..)
   , Associativity(..)
-  )
-  where
+  ) where
 
-
-import Prelude hiding (Either(..))
-import Control.Monad (liftM)
-import Data.Binary
-
-
+import           Control.Monad (liftM)
+import           Data.Binary
+import           Prelude       hiding (Either (..))
 
 -- BINOP STUFF
-
-
-newtype Precedence = Precedence Int
+newtype Precedence =
+  Precedence Int
   deriving (Eq, Ord)
-
 
 data Associativity
   = Left
@@ -25,31 +20,22 @@ data Associativity
   | Right
   deriving (Eq)
 
-
-
 -- BINARY
-
-
 instance Binary Precedence where
-  get =
-    liftM Precedence get
-
-  put (Precedence n) =
-    put n
-
+  get = liftM Precedence get
+  put (Precedence n) = put n
 
 instance Binary Associativity where
-  get =
-    do  n <- getWord8
-        case n of
-          0 -> return Left
-          1 -> return Non
-          2 -> return Right
-          _ -> fail "Error reading valid associativity from serialized string"
-
+  get = do
+    n <- getWord8
+    case n of
+      0 -> return Left
+      1 -> return Non
+      2 -> return Right
+      _ -> fail "Error reading valid associativity from serialized string"
   put assoc =
     putWord8 $
-      case assoc of
-        Left  -> 0
-        Non   -> 1
-        Right -> 2
+    case assoc of
+      Left  -> 0
+      Non   -> 1
+      Right -> 2
