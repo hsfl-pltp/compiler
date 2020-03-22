fa51be0bdc1c6a18ac851ead5c38d6a946d65a08
Ken Merten Blumenthal <ken.blumenthal@web.de>
2020-03-22 15:10:01 +0100

    Update Expression.hs
    Die ersten vier Fälle definiert - Float muss noch getestet werden wenn der KOnstruktor in der Generate.C.Builder.hs entsprechend angepasst wurde

--- compiler/src/Generate/C/Expression.hs
+++ compiler/src/Generate/C/Expression.hs
@@ -4,24 +4,38 @@ module Generate.C.Expression
   ()
   where
 
-
 import qualified Generate.C.Builder as C
 import qualified AST.Optimized as Opt
 
+import qualified Elm.String as ES
+import qualified Elm.Float as EF
 
--- generiert den Ausdruck des vorliegenden Datentypen
+import qualified Data.Utf8 as Utf8
+
+
+-- generates Expression for Type
 generate :: Opt.Expr -> C.Expr
 generate expr =
   case expr of
     Opt.Bool bool ->
-      return CExpr (C.Bool bool)
-    Opt.String string ->
-      return CExpr (C.String string)
+      C.Bool bool
+    Opt.Str string ->
+      C.String (convertString string)
     Opt.Int int ->
-      return CExpr (C.Integer int)
-    Opt.Double double ->
-      return CExpr (C.Double double)
+      C.Double (convertInt int)
+-- uncomment when Double type is adjusted
+    --Opt.Float float ->
+      --C.String (convertFloat float)
 
 
-data Code
-  = CExpr C.Expr
+convertString :: ES.String -> String
+convertString string =
+  Utf8.toChars string
+
+convertInt :: Int -> Double
+convertInt int =
+  fromIntegral int
+
+--convertFloat :: EF.Float -> String
+--convertFloat float =
+  --Utf8.toChars float
