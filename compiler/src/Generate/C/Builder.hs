e9ad6c6bfe9d53d76d30b96d2a5360f7746a0083
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2020-04-08 08:28:19 +0200

    Add missing cases in pretty printer

--- compiler/src/Generate/C/Builder.hs
+++ compiler/src/Generate/C/Builder.hs
@@ -1,117 +1,117 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Generate.C.Builder
-  (Expr(..), pretty, Stmt(..), PrefixOp(..))
-  where
-import Data.Typeable
-import Data.List
-import Data.ByteString.Builder as B
-import qualified Data.List as List
+  ( Expr(..)
+  , pretty
+  , Stmt(..)
+  , PrefixOp(..)
+  ) where
+
+import           Data.ByteString.Builder as B
+import qualified Data.List               as List
 
 -- Expressions
 data Expr
-    = String Builder
-    | Comma [Expr]
-    | Null 
-    | Bool Bool
-    | Integer Builder
-    | Double Builder
-    | If Expr Expr Expr
-    | While Expr Expr Expr
-    | Prefix PrefixOp Expr
-    | Infix InfixOp Expr Expr
-
-
+  = String Builder
+  | Comma [Expr]
+  | Null
+  | Bool Bool
+  | Integer Builder
+  | Double Builder
+  | If Expr Expr Expr
+  | While Expr Expr Expr
+  | Prefix PrefixOp Expr
+  | Infix InfixOp Expr Expr
 
 -- STATEMENTS
-
 data Stmt
-    = Block [Stmt]
-    | EmptyStmt
-    | Var String Builder Expr
-    | Decl String Builder
-    | Const Expr
-    | IfStmt Expr Stmt Stmt
-    | WhileStmt Expr Stmt
-    | Function String Builder Stmt Stmt -- first Stmt is CommaStmt
-    | CommaStmt [Stmt] -- [Stmt] is Decl
-
-
+  = Block [Stmt]
+  | EmptyStmt
+  | Var String Builder Expr
+  | Decl String Builder
+  | Const Expr
+  | IfStmt Expr Stmt Stmt
+  | WhileStmt Expr Stmt
+  | Function String Builder Stmt Stmt -- first Stmt is CommaStmt
+  | CommaStmt [Stmt] -- [Stmt] is Decl
 
 -- Converts a datatype in form of a String to the equivelant C-datatype.
 -- Also returned as a String.
 prettyDataType :: String -> Builder
-prettyDataType dataType  =
+prettyDataType dataType =
   case dataType of
-    "Integer" -> "int" 
-    "Double" ->  "double" 
-    "String" -> "string"  
-    "Bool" -> "bool"  
-    "Void" -> "void"
-  
+    "Integer" -> "int"
+    "Double"  -> "double"
+    "String"  -> "string"
+    "Bool"    -> "bool"
+    "Void"    -> "void"
+
 --This function takes a Stmt and converts it into a C-program as a string.
 pretty :: Stmt -> Builder
 pretty statement =
   case statement of
     Var dataType name expr ->
-      mconcat [(prettyDataType dataType) , " " , name , " = " , (prettyExpr expr) , ";\n"]
-    Block array ->
-      mconcat (map pretty array)
-    Const constExpr ->
-      mconcat ["const" , (prettyExpr constExpr) , ";\n"]
-    Decl dataType name ->
-      mconcat [(prettyDataType dataType) , " " , name]
+      mconcat
+        [(prettyDataType dataType), " ", name, " = ", (prettyExpr expr), ";\n"]
+    Block array -> mconcat (map pretty array)
+    Const constExpr -> mconcat ["const", (prettyExpr constExpr), ";\n"]
+    Decl dataType name -> mconcat [(prettyDataType dataType), " ", name]
     IfStmt condition thenStmt elseStmt ->
       mconcat
-        ["if(", (prettyExpr condition), ") {\n"
-        , (pretty thenStmt)
+        [ "if("
+        , prettyExpr condition
+        , ") {\n"
+        , pretty thenStmt
         , "} else {\n"
-        , (pretty elseStmt) ,"}\n"
+        , pretty elseStmt
+        , "}\n"
         ]
     WhileStmt condition loopStmt ->
       mconcat
-       ["while(", (prettyExpr condition), ") {\n"
-       , (pretty loopStmt)
-       , "}"
-       ]
-    CommaStmt commastmt -> 
-        mconcat (List.intersperse ", " (map pretty commastmt))
+        ["while(", (prettyExpr condition), ") {\n", (pretty loopStmt), "}"]
+    CommaStmt commastmt ->
+      mconcat (List.intersperse ", " (map pretty commastmt))
     Function dataType name commastmt body ->
       mconcat
-        [(prettyDataType dataType)
-        , " " , name , "("
-        , (pretty commastmt)
+        [ prettyDataType dataType
+        , " "
+        , name
+        , "("
+        , pretty commastmt
         , ") {"
-        , (pretty body)
+        , pretty body
         , "}"
         ]
-     
-      
-
+    EmptyStmt -> error "Not supported EmptyStmt"
 
 --Converts an argument of the type Expr into a String.
 prettyExpr :: Expr -> Builder
 prettyExpr expression =
   case expression of
-    
     Bool bool ->
-     case bool of
-       True ->  "true"
-       False -> "false"
-
-    If infixExpr expr1 expr2 -> mconcat [(prettyExpr expr1) , " " , (prettyExpr infixExpr) , " " , (prettyExpr expr2)]
-    
+      case bool of
+        True  -> "true"
+        False -> "false"
+    If infixExpr expr1 expr2 ->
+      mconcat
+        [prettyExpr expr1, " ", prettyExpr infixExpr, " ", prettyExpr expr2]
     Null -> "null"
-
     Integer integer -> integer
-
     Double double -> double
-
-    Infix infixoperator expr1 expr2 -> mconcat [(prettyExpr expr1) , " " , (prettyInfix infixoperator) , " " , (prettyExpr expr2)]
-
-    Prefix prefixOperator expr1 -> mconcat [(prettyPrefix prefixOperator) , (prettyExpr expr1)]
-
-
+    Infix infixoperator expr1 expr2 ->
+      mconcat
+        [ prettyExpr expr1
+        , " "
+        , prettyInfix infixoperator
+        , " "
+        , prettyExpr expr2
+        ]
+    Prefix prefixOperator expr1 ->
+      mconcat [prettyPrefix prefixOperator, prettyExpr expr1]
+    String _ -> error "Not supported String"
+    Comma _ -> error "Not supported Comma"
+    While _ _ _ -> error "Not supported While"
 
 data InfixOp
   = OpAdd -- +
--- compiler/src/Generate/C/Example.hs
+++ compiler/src/Generate/C/Example.hs
@@ -1,25 +1,54 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Generate.C.Example
-  (test1)
-  where
+  ( test1
+  , test2
+  , test3
+  ) where
+
+import           Data.ByteString.Builder   as B
+import           Data.ByteString.Lazy.UTF8 (toString)
 
 --Beispiel Konstruktor um die pretty Funktion zu testen.
-import Generate.C.Builder 
-import Data.ByteString.Builder as B
-import Data.Word
+import           Generate.C.Builder
 
-import qualified Data.ByteString.Lazy as L
-builderToString :: Builder ->  L.ByteString
-builderToString builder =
-  B.toLazyByteString builder
+import qualified Data.ByteString.Lazy      as L
 
-lazyStringToWord :: L.ByteString -> [Word8]
-lazyStringToWord bs = L.unpack bs
+builderToString :: Builder -> L.ByteString
+builderToString builder = B.toLazyByteString builder
 
 example1 :: Stmt
-example1 = Block [Function "Void"  "testFunc" (CommaStmt [(Decl "Bool" "boolean"),(Decl "Bool" "boolean2") ]) (Var "String" "str" (String "hi")) ,Var "String" "str" (String "hi"), Var "Bool" "boolean" (Bool False), IfStmt (Prefix PrefixNot (Bool True)) (Var "String" "str" (String "hi")) (Var "String" "str" (String "hi"))]
+example1 = Var "String" "x" (Bool False)
 
+test1 :: String
+test1 = toString (builderToString (pretty example1))
 
-test1 :: [Word8]
-test1 = lazyStringToWord(builderToString (pretty example1))
+example2 :: Stmt
+example2 =
+  IfStmt
+    (Prefix PrefixNot (Bool True))
+    (Var "String" "x" (Bool False))
+    (Var "String" "str" (String "hi"))
+
+test2 :: String
+test2 = toString (builderToString (pretty example2))
+
+example3 :: Stmt
+example3 =
+  Block
+    [ Function
+        "Void"
+        "testFunc"
+        (CommaStmt [(Decl "Bool" "boolean"), (Decl "Bool" "boolean2")])
+        (Var "String" "str" (String "hi"))
+    , Var "String" "str" (String "hi")
+    , Var "Bool" "boolean" (Bool False)
+    , IfStmt
+        (Prefix PrefixNot (Bool True))
+        (Var "String" "str" (String "hi"))
+        (Var "String" "str" (String "hi"))
+    ]
+
+test3 :: String
+test3 = toString (builderToString (pretty example3))
