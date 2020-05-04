5fc77807ba290c8c3390dd4fa4645e2a08387486
arljba <33658992+arljba@users.noreply.github.com>
2020-05-05 00:04:04 +0200

    Add Code for If generation
    Use the functions defined for if generation with JS as a template.
    Change the functions to generate C-Code.

--- compiler/src/Generate/Arduino/Expression.hs
+++ compiler/src/Generate/Arduino/Expression.hs
@@ -17,6 +17,10 @@ import qualified Elm.String as ES
 
 import qualified Data.Utf8 as Utf8
 
+generateArduinoExpr :: Opt.Expr -> C.Expr
+generateArduinoExpr expression =
+  codeToExpr (generate expression)
+
 -- generates Expression for Type
 generate :: Opt.Expr -> Code
 generate expr =
@@ -25,6 +29,7 @@ generate expr =
     Opt.Str string -> CExpr (C.String (convertString string))
     Opt.Int int -> CExpr (C.Double (convertInt int))
     Opt.Float float -> CExpr (C.Double (convertFloat float))
+    Opt.If branches final -> generateIf branches final
     _ -> error (show expr)
 
 data Code
@@ -47,3 +52,67 @@ convertInt int = B.intDec int
 
 convertFloat :: EF.Float -> B.Builder
 convertFloat float = Utf8.toBuilder float
+
+
+-- GENERATE IFS
+
+generateIf :: [(Opt.Expr, Opt.Expr)] -> Opt.Expr -> Code
+generateIf givenBranches givenFinal =
+  let
+    (branches, final) =
+      crushIfs givenBranches givenFinal
+
+    convertBranch (condition, expr) =
+      ( generateArduinoExpr condition
+      , generate expr
+      )
+
+    branchExprs = map convertBranch branches
+    finalCode = generate final
+  in
+  if isBlock finalCode || any (isBlock . snd) branchExprs then
+    CBlock [ foldr addStmtIf (codeToStmt finalCode) branchExprs ]
+  else
+    CExpr $ foldr addExprIf (codeToExpr finalCode) branchExprs
+
+
+addExprIf :: (C.Expr, Code) -> C.Expr -> C.Expr
+addExprIf (condition, branch) final =
+  C.If condition (codeToExpr branch) final
+
+
+addStmtIf :: (C.Expr, Code) -> C.Stmt -> C.Stmt
+addStmtIf (condition, branch) final =
+  C.IfStmt condition (codeToStmt branch) final
+
+
+isBlock :: Code -> Bool
+isBlock code =
+  case code of
+    CBlock _ -> True
+    CExpr _ -> False
+
+
+crushIfs :: [(Opt.Expr, Opt.Expr)] -> Opt.Expr -> ([(Opt.Expr, Opt.Expr)], Opt.Expr)
+crushIfs branches final =
+  crushIfsHelp [] branches final
+
+
+crushIfsHelp
+    :: [(Opt.Expr, Opt.Expr)]
+    -> [(Opt.Expr, Opt.Expr)]
+    -> Opt.Expr
+    -> ([(Opt.Expr, Opt.Expr)], Opt.Expr)
+crushIfsHelp visitedBranches unvisitedBranches final =
+  case unvisitedBranches of
+    [] ->
+        case final of
+          Opt.If subBranches subFinal ->
+              crushIfsHelp visitedBranches subBranches subFinal
+
+          _ ->
+              (reverse visitedBranches, final)
+
+    visiting : unvisited ->
+        crushIfsHelp (visiting : visitedBranches) unvisited final
+ 
