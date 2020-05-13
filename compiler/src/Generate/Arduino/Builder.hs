f6157899a5addbc32c8b70bf12b1d638ea0a9d55
Ken Merten Blumenthal <ken.blumenthal@web.de>
2020-05-13 10:13:06 +0200

    Clean up Code

--- compiler/src/Generate/Arduino/Builder.hs
+++ compiler/src/Generate/Arduino/Builder.hs
@@ -17,7 +17,6 @@ import Generate.Arduino.Name (Name)
 -- Expressions
 data Expr
   = String Builder
-  | Comma [Expr]
   | Null
   | Ref Name
   | Bool Bool
@@ -42,17 +41,16 @@ data Stmt
   | IfStmt Expr Stmt Stmt
   | WhileStmt Expr Stmt
   | FunctionStmt Name [Name] [Stmt]
-  | CommaStmt [Stmt] -- [Stmt] is Decl
 
 -- Converts a datatype in form of a String to the equivelant C-datatype.
 -- Also returned as a String.
 prettyDataType :: String -> Builder
 prettyDataType dataType =
   case dataType of
-    "Integer" -> "int"
-    "Double" -> "double"
     "String" -> "string"
     "Bool" -> "bool"
+    "Integer" -> "int"
+    "Double" -> "double"
     "Void" -> "void"
     -- Dummy case used because type information is missing
     "any" -> "any"
@@ -61,12 +59,14 @@ prettyDataType dataType =
 pretty :: Stmt -> Builder
 pretty statement =
   case statement of
+    Block array -> mconcat (map pretty array)
+    EmptyStmt -> error "Not supported EmptyStmt"
     Var dataType name expr ->
       mconcat
         [(prettyDataType dataType), " ", name, " = ", (prettyExpr expr), ";\n"]
-    Block array -> mconcat (map pretty array)
-    Const constExpr -> mconcat ["const", (prettyExpr constExpr), ";\n"]
     Decl dataType name -> mconcat [(prettyDataType dataType), " ", name]
+    Const constExpr -> mconcat ["const", (prettyExpr constExpr), ";\n"]
+    Return expr -> mconcat ["return ", (prettyExpr expr)]
     IfStmt condition thenStmt elseStmt ->
       mconcat
         [ "if("
@@ -77,13 +77,9 @@ pretty statement =
         , pretty elseStmt
         , "}\n"
         ]
-    Return expr -> mconcat ["return ", (prettyExpr expr)]
     WhileStmt condition loopStmt ->
       mconcat
         ["while(", (prettyExpr condition), ") {\n", (pretty loopStmt), "}"]
-    CommaStmt commastmt ->
-      mconcat (List.intersperse ", " (map pretty commastmt))
-    EmptyStmt -> error "Not supported EmptyStmt"
     FunctionStmt name args stmts ->
       mconcat
         [ "void "
@@ -102,18 +98,23 @@ fromStmtBlock stmts = mconcat (map pretty stmts)
 prettyExpr :: Expr -> Builder
 prettyExpr expression =
   case expression of
+    String _ -> error "Not supported String"
+    Null -> "null"
+    Ref name -> Name.toBuilder name
     Bool bool ->
       case bool of
         True -> "true"
         False -> "false"
+    Integer integer -> integer
+    Int n -> B.intDec n
+    Double double -> double
     If infixExpr expr1 expr2 ->
       mconcat
         [prettyExpr expr1, " ", prettyExpr infixExpr, " ", prettyExpr expr2]
-    Null -> "null"
-    Int n -> B.intDec n
-    Ref name -> Name.toBuilder name
-    Integer integer -> integer
-    Double double -> double
+    While _ _ _ -> error "Not supported While"
+    Prefix prefixOperator expr1 ->
+      mconcat [prettyPrefix prefixOperator, prettyExpr expr1]
+    Call _ _ -> error "Not supported Call"
     Infix infixoperator expr1 expr2 ->
       mconcat
         [ prettyExpr expr1
@@ -122,11 +123,6 @@ prettyExpr expression =
         , " "
         , prettyExpr expr2
         ]
-    Prefix prefixOperator expr1 ->
-      mconcat [prettyPrefix prefixOperator, prettyExpr expr1]
-    String _ -> error "Not supported String"
-    Comma _ -> error "Not supported Comma"
-    While _ _ _ -> error "Not supported While"
     Function maybeName args stmts ->
       mconcat
         [ "void "
