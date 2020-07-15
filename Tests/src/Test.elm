ee94658bd2984b26dfefc6b62e99ab3f0098d0f6
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2020-07-15 08:43:59 +0200

    Collect test cases

--- Tests/src/Test.elm
+++ Tests/src/Test.elm
@@ -1,10 +1,35 @@
 module Test exposing (..)
 
 
-main : Int
-main =
+test1 : Int
+test1 =
+    1 + 2
+
+
+test2 : Int
+test2 =
+    1 * 2
+
+
+test3 : Int
+test3 =
     if False then
         1
 
     else
         0
+
+
+test4 : Int
+test4 =
+    id 42
+
+
+id : Int -> Int
+id i =
+    i
+
+
+main : Int
+main =
+    test1 + test2 + test3 + test4
